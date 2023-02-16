f3aeab73c8a2ed2c670f36011ef345c354ca474e
Arko04 <karimialireza2004@gmail.com>
2023-02-17 00:19:12 +0330

    updated

--- AP/A/1/12.cpp
+++ AP/A/1/12.cpp
@@ -6,19 +6,18 @@
 
 using namespace std;
 
-#define ZERO 0
-
 #define DAY 0
 #define MONTH 1
 #define YEAR 2
 
+#define DATE_DELIMITER '/'
 #define MAX_LEN 20
 
 typedef struct note
 {
-    vector<int> date{ZERO, ZERO, ZERO};  // DAY, MONTH, YEAR
-    int cnt_pwords = ZERO;
-    int sizeStr = ZERO;
+    vector<int> date{DAY, MONTH, YEAR};
+    int positive_word_count = 0;
+    int diary_length = 0;
     string diary = "";
 } note;
 
@@ -27,17 +26,17 @@ vector<string> reading_file_positive_words(string filename);
 int count_positive_words(string line, vector<string> positive_words);
 int count_word_in_text(string line, string word);
 
-void dateInit(vector<int> &time);
+void date_initialize(vector<int> &time);
 
-note checkDate(vector<note> notepad, vector<int> time);
+note check_date(vector<note> notepad, vector<int> time);
 
-note checkLongest(vector<note> notepad);
+note check_longest_note(vector<note> notepad);
 
-note checkBest(vector<note> notepad);
+note check_best_code(vector<note> notepad);
 
-void printNote(note text);
+void print_note(note text);
 
-void addNote(string input, vector<string> positve_words, vector<note> &notepad);
+void add_note(string input, vector<string> positve_words, vector<note> &notepad);
 
 int main(void)
 {
@@ -52,30 +51,34 @@ int main(void)
         if (input == "start_day")
         {
             notepad.resize(notepad.size() + 1);
-            dateInit(notepad.back().date);
+            date_initialize(notepad.back().date);
         }
         else if (input == "show_day")
         {
-            dateInit(temp_day);
-            check_note = checkDate(notepad, temp_day);
+            date_initialize(temp_day);
+            check_note = check_date(notepad, temp_day);
             cout << check_note.diary;
         }
         else if (input == "show_the_longest_day")
         {
-            check_note = checkLongest(notepad);
-            cout << check_note.date[DAY] << '/' << check_note.date[MONTH] << '/' << check_note.date[YEAR] << '\n';
-            printNote(check_note);
+            check_note = check_longest_note(notepad);
+            cout << check_note.date[DAY] << DATE_DELIMITER << 
+                    check_note.date[MONTH] << DATE_DELIMITER << 
+                    check_note.date[YEAR] << endl;
+            print_note(check_note);
         }
         else if (input == "show_the_best_day")
         {
-            check_note = checkBest(notepad);
-            cout << check_note.date[DAY] << '/' << check_note.date[MONTH] << '/' << check_note.date[YEAR] << '\n';
-            printNote(check_note);
+            check_note = check_best_code(notepad);
+            cout << check_note.date[DAY] << DATE_DELIMITER << 
+                    check_note.date[MONTH] << DATE_DELIMITER << 
+                    check_note.date[YEAR] << endl;
+            print_note(check_note);
         }
         else
-            addNote(input, positive_words, notepad);
+            add_note(input, positive_words, notepad);
     }
-    return 0;
+    exit(EXIT_SUCCESS);
 }
 
 vector<string> reading_file_positive_words(string filename) // positive-words.txt
@@ -103,18 +106,18 @@ int count_word_in_text(string text, string word)
     return distance(words_begin, words_end);
 }
 
-void dateInit(vector<int> &time)
+void date_initialize(vector<int> &time)
 {
     getchar();
-    vector<int> dates(3, ZERO);
-    int i = ZERO;
-    char c;
+    vector<int> dates(3, 0);
+    int i = 0;
+    char letter;
 
-    while (cin.get(c))
+    while (cin.get(letter))
     {
-        switch (c)
+        switch (letter)
         {
-        case '/':
+        case DATE_DELIMITER:
             time[i] = dates[i];
             i++;
             break;
@@ -123,7 +126,7 @@ void dateInit(vector<int> &time)
             time[i] = dates[i];
             return;
         default:
-            dates[i] = dates[i] * 10 + (c - '0');
+            dates[i] = dates[i] * 10 + (letter - '0');
             break;
         }
     }
@@ -132,17 +135,17 @@ void dateInit(vector<int> &time)
 
 int count_positive_words(string line, vector<string> positive_words)
 {
-    transform(line.begin(), line.end(), line.begin(),::tolower); /// converting to lowercase
+    transform(line.begin(), line.end(), line.begin(), ::tolower);
 
-    int cnt_pwords = ZERO;
+    int positive_word_count = 0;
     for (string word : positive_words)
     {
-        cnt_pwords += count_word_in_text(line, word);
+        positive_word_count += count_word_in_text(line, word);
     }
-    return cnt_pwords;
+    return positive_word_count;
 }
 
-note checkDate(vector<note> notepad, vector<int> time)
+note check_date(vector<note> notepad, vector<int> time)
 {
     for (note diary : notepad)
     {
@@ -154,41 +157,41 @@ note checkDate(vector<note> notepad, vector<int> time)
     exit(EXIT_FAILURE);
 }
 
-note checkLongest(vector<note> notepad)
+note check_longest_note(vector<note> notepad)
 {
-    int max_val = ZERO;
+    int max_size = 0;
     note longest_diary;
 
     for (note temp_diary : notepad)
     {
-        if (temp_diary.sizeStr > max_val)
+        if (temp_diary.diary_length > max_size)
         {
-            max_val = temp_diary.sizeStr;
+            max_size = temp_diary.diary_length;
             longest_diary = temp_diary;
         }
     }
     return longest_diary;
 }
 
-note checkBest(vector<note> notepad)
+note check_best_code(vector<note> notepad)
 {
-    int max_val = ZERO;
+    int max_positive_words = 0;
     note best_diary;
 
     for (note temp_diary : notepad)
     {
-        if (temp_diary.cnt_pwords > max_val)
+        if (temp_diary.positive_word_count > max_positive_words)
         {
-            max_val = temp_diary.cnt_pwords;
+            max_positive_words = temp_diary.positive_word_count;
             best_diary = temp_diary;
         }
     }
     return best_diary;
 }
 
-void printNote(note text)
+void print_note(note text)
 {
-    int cnt_letters = ZERO;
+    int cnt_letters = 0;
     vector<char> letters(text.diary.begin(), text.diary.end());
 
     for (char ch : letters)
@@ -204,18 +207,18 @@ void printNote(note text)
             return;
         }
     }
-    cout << '\n';
+    cout << endl;
     return;
 }
 
-void addNote(string input, vector<string> positive_words, vector<note> &notepad)
+void add_note(string input, vector<string> positive_words, vector<note> &notepad)
 {
     string line = input;
     getline(cin, input);
     line += input;
 
-    notepad.back().sizeStr += line.size();
-    notepad.back().cnt_pwords += count_positive_words(line, positive_words);
+    notepad.back().diary_length += line.size();
+    notepad.back().positive_word_count += count_positive_words(line, positive_words);
 
     notepad.back().diary += line;
     notepad.back().diary += '\n';
