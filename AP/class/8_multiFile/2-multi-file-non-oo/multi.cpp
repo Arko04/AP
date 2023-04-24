7b131a5bdd18032ccd7470eb9193d9eea50cf45d
Arko04 <karimialireza2004@gmail.com>
2023-04-24 11:36:31 +0330

    Add event_driven

---
+++ AP/class/8_multiFile/2-multi-file-non-oo/multi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "strutil.hpp"
+using namespace std;
+
+int main() {
+	cout << reverse("hello") << endl;
+	cout << toUpper("Ramtin") << endl;
+	cout << toInteger("124") << endl;
+}
+
